b95792d6740e4efc862150d3a4db55cb12f1e7a5
RemoIsThere <102456519+RemoIsThere@users.noreply.github.com>
2022-09-10 18:32:31 +0530

    Add files via upload

--- Blinklastname.ino
+++ Blinklastname.ino
@@ -12,47 +12,20 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   // P
-  dot();
-  dash();
-  dash();
-  dot();
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
-  delay(2000);
+      alphaP();
   
 //  a
-   dot();
-   dash();
-   digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
-  delay(2000);
+   alphaA();
 //  p
-  dot();
-  dash();
-  dash();
-  dot();
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
-  delay(2000);
-  
+    alphaP();
 //  n
-  dash();
-  dot();
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
-  delay(2000);
+    alphaN();
 //  e
-  dot();
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
-  delay(2000);
+    alphaE();
 //  j
-  dot();
-  dash();
-  dash();
-  dash();
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
-  delay(2000);
+    alphaJ();
 //  a
-  dot();
-  dash();
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
-  delay(20000);
+    alphaA();
 }
 
 void dot(){
@@ -67,3 +40,192 @@ void dash(){
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);  
   }
+
+  
+//alphabets
+void alphaA(){
+  dot();
+   dash();
+   digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);
+}
+void alphaB(){
+  dash();
+  dot();
+  dot();
+  dot();
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);
+}
+void alphaC(){
+  dash();
+  dot();
+  dash();
+  dot();
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);
+}
+void alphaD(){
+  dash();
+  dot();
+  dot();
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);
+}
+void alphaE(){
+    dot();
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);
+}
+void alphaF(){
+  dot();
+  dot();
+  dash();
+  dot();
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);
+}
+void alphaG(){
+  dash();
+  dash();
+  dot();
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);
+}
+void alphaH(){
+  dot();
+  dot();
+  dot();
+  dot();
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);
+}
+void alphaI(){
+  dot();
+  dot();
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);
+}
+void alphaJ(){
+    dot();
+  dash();
+  dash();
+  dash();
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);
+}
+void alphaK(){
+  dash();
+  dot();
+  dash();
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);
+}
+void alphaL(){
+  dash();
+  dot();
+  dot();
+  dot();
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);
+}
+void alphaM(){
+  dash();
+  dash();
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);
+}
+void alphaN(){
+    dash();
+  dot();
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);
+}
+void alphaO(){
+  dash();
+  dash();
+  dash();
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);
+}
+void alphaP(){
+    dot();
+  dash();
+  dash();
+  dot();
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);
+}
+void alphaQ(){
+  dash();
+  dash();
+  dot();
+  dash();
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);
+}
+void alphaR(){
+  dot();
+  dash();
+  dot();
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);
+}
+void alphaS(){
+  dot();
+  dot();
+  dot();
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);
+}
+void alphaT(){
+  dash();
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);
+}
+void alphaU(){
+  dot();
+  dot();
+  dash();
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);
+}
+void alphaV(){
+  dot();
+  dot();
+  dot();
+  dash();
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);
+}
+void alphaW()
+  dot();
+  dash();
+  dash();
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);
+}
+void alphaX(){
+  dash();
+  dot();
+  dot();
+  dash();
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);
+}
+void alphaY(){
+  dash();
+  dot();
+  dash();
+  dash();
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);
+}
+void alphaZ(){
+  dash();
+  dash();
+  dot();
+  dot();
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);
+}
